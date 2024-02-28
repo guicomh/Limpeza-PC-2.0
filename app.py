8bc40cc00d90f52141e547ef5fe135706e13f4ed
Guilherme <eliseuvasconcellos@gmail.com>
2024-02-28 12:12:37 -0300

    update

---
+++ app.py
@@ -0,0 +1,26 @@
+#
+#   Software Desenvolvido por https://github.com/guicomh
+#
+
+
+from tkinter import *
+from tkinter import ttk
+import main
+from PIL import Image, ImageTk
+
+
+def apagar():
+
+    main.limpar_pasta()
+    main.limpar_pasta_temp()
+
+janela = Tk()
+
+janela.resizable(False, False)
+janela.geometry("800x600")
+janela.title("Limpeza 2.0")
+
+botao_apagar = Button(janela, text="Limpar", command=apagar) . pack()
+
+
+janela.mainloop()
---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+def limpar_pasta_temp():
+
+    os.system("rd/s/q %temp%")
+    return 
+
+
+def limpar_pasta():
+
+    os.system("start cmd && color a2")
+    return
+
+
+
+
